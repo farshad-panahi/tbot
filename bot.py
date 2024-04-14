b5de8666406b25bbca61829835437b71528b1e76
farshad panahi <feri.93.p@gmail.com>
2024-04-14 17:20:12 +0500

    inlineQuery

--- bot.py
+++ bot.py
@@ -1,26 +1,53 @@
 import logging
+from decouple import config
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-from decouple import config
+
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 
-async def start(
-        update: Update,
-        context: ContextTypes.DEFAULT_TYPE
-        ):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
     )
 
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram.ext import ApplicationBuilder, ContextTypes, InlineQueryHandler
+
+
+async def inlineHandle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    inline_request = update.inline_query
+    if not inline_request:
+        return
+
+    query = inline_request.query
+    if not query:
+        return
+
+    responses = [
+        InlineQueryResultArticle(
+            id="1",
+            title="UpperCase",
+            input_message_content=InputTextMessageContent(query.upper()),
+        ),
+        InlineQueryResultArticle(
+            id="2",
+            title="LowerCase",
+            input_message_content=InputTextMessageContent(query.lower()),
+        ),
+    ]
+
+    await inline_request.answer(responses)
+
 
 if __name__ == "__main__":
-    application = ApplicationBuilder().token(config('TELEGRAM_API_TOKEN')).build()
+    token = config("TELEGRAM_API_TOKEN")
+    application = ApplicationBuilder().token(token).build()
 
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
-
+    application.add_handler(InlineQueryHandler(inlineHandle))
     application.run_polling()
