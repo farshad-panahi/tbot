4a105dd27450d881de1c0cea31387af47041aee6
farshad panahi <feri.93.p@gmail.com>
2024-04-16 01:24:53 +0500

    game of throne inline query character searching

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # MARK:imports
 import logging
 from typing import Final
-from telegram import Update
-from telegram.ext import Application, ContextTypes, CommandHandler
+from uuid import uuid4
+from telegram import InlineQueryResultPhoto, Update
+from telegram.ext import Application, ContextTypes, CommandHandler, InlineQueryHandler
 from decouple import config
 import requests
 
@@ -16,36 +17,39 @@ def main():
     logging.getLogger("httpx").setLevel(logging.WARNING)
     logger = logging.getLogger(__name__)
 
-    async def add_c_handler(
+    async def inline_query(
             update:Update,
             context:ContextTypes.DEFAULT_TYPE
     ):
-        m, n = int(context.args[0]), int(context.args[1])
+        query = update.inline_query.query
+        data = requests.get("https://thronesapi.com/api/v2/Characters")
+        data = data.json()
+        characters = {}
+        for character in data:
+            characters[character["fullName"]] = character["imageUrl"]
+        if not query:
+            results = []
 
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=f"{n} + {m} = {n + m}"
-        )
-
-    async def mult_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        n, m = int(context.args[0]), int(context.args[1])
-
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=f"{n} + {m} = {n * m}"
-        )
-
-    async def ex_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        exp = context.args
-        resp = " ".join(i for i in exp)
-
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, text=f"{resp} = {eval(resp)}",
-            reply_to_message_id=update.effective_message.id
-        )
+            for name, url in characters.items():
+                newItem = InlineQueryResultPhoto(
+                    id=str(uuid4()),
+                    photo_url=url,
+                    thumbnail_url=url,
+                    caption=name
+                )
+                results.append(newItem)
+        else:
+            results = []
+            for name, url in characters.items():
+                if query in name:
+                    newItem = InlineQueryResultPhoto(
+                        id=str(uuid4()), photo_url=url, thumbnail_url=url, caption=name
+                    )
+                    results.append(newItem)
+        await update.inline_query.answer(results, auto_pagination=True)
 
     bot = Application.builder().token(TOKEN).build()
-    bot.add_handler(CommandHandler("add", add_c_handler))
-    bot.add_handler(CommandHandler("mult", mult_handler))
-    bot.add_handler(CommandHandler("ex", ex_handler))
+    bot.add_handler(InlineQueryHandler(inline_query))
     bot.run_polling()
 
 if __name__ == "__main__":
